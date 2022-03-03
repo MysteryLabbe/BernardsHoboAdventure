823f9ade493d2e6ac270d33e94faeda8453b9451
PlasticPack <gabriel.bouchard3@usherbrooke.ca>
2022-03-03 15:37:14 -0500

    Finished movement and camera for player and gravity prefabs created

--- Bernards Hobo Adventure/Assets/Scripts/movePlayer.cs
+++ Bernards Hobo Adventure/Assets/Scripts/movePlayer.cs
@@ -11,7 +11,13 @@ public class movePlayer : MonoBehaviour
     private float lookX;
     private float lookY;
 
-    public float speed = 1;
+    public float speed = 6f;
+    public float rotationSmoothTime = 0.1f;
+    public float gravity = -9.81f;
+
+    public Transform cam;
+
+    private float currentAngleVelocity = 0f;
     private CharacterController controller;
     // Start is called before the first frame update
     void Start()
@@ -27,28 +33,28 @@ public class movePlayer : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
 
-
     }
 
     private void OnLook(InputValue lookValue)
     {
-        Vector2 lookVector = lookValue.Get<Vector2>();
 
-        lookX = lookVector.x;
-        lookY = lookVector.y;
-
-        Vector3 look = new Vector3(lookX, 0, lookY);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 movement = new Vector3(movementX, 0, movementY);
-        controller.Move(movement * speed);
-    }
+        //gravity
+        controller.Move(new Vector3 (0f,gravity * Time.deltaTime, 0f));
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 movement = new Vector3(movementX, 0f, movementY);
+        if (movement.magnitude > 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(movementX, movementY) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentAngleVelocity, rotationSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 direction = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 motion = direction * speed * Time.deltaTime;
+            controller.Move(motion);
+        }
     }
 }
